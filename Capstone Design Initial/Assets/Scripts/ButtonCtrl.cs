72c1cefd53211d61d879966ea0c6623ec6e99601
jangho1016 <dhwkdgh12@gmail.com>
2022-03-21 18:27:46 +0900

    commit

--- Capstone Design Initial/Assets/Scripts/ButtonCtrl.cs
+++ Capstone Design Initial/Assets/Scripts/ButtonCtrl.cs
@@ -7,11 +7,30 @@ public class ButtonCtrl : MonoBehaviour
 {
     public void StartBT()
     {
+        PlayerPrefs.SetInt("SceneNum", 1);
+        SceneManager.LoadScene("Select room");
+    }
+
+    public void MainBT()
+    {
+        PlayerPrefs.SetInt("SceneNum", 0);
+        SceneManager.LoadScene("Start Scene");
+    }
+
+    public void Information()
+    {
+        PlayerPrefs.SetInt("SceneNum", 0);
         SceneManager.LoadScene("Information");
     }
 
     public void ExitBT()
     {
         Application.Quit();
+    }
+
+    public void SelectBT()
+    {
+        PlayerPrefs.SetInt("SceneNum", 2);
+        SceneManager.LoadScene("Room");
     }
 }
--- Capstone Design Initial/Assets/Scripts/PlayerCtrl.cs
+++ Capstone Design Initial/Assets/Scripts/PlayerCtrl.cs
@@ -9,6 +9,13 @@ public class PlayerCtrl : MonoBehaviour
     public GameObject mainCam;
     private float GaugeTimer = 0.0f;
     private float gazeTimer = 2.0f;
+    private float positionY;
+
+    void Start()
+    {
+        PlayerPrefs.SetInt("SceneNum", 0);
+        Debug.Log(PlayerPrefs.GetInt("SceneNum"));
+    }
 
     void Update()
     {
@@ -22,8 +29,25 @@ public class PlayerCtrl : MonoBehaviour
 
             if (GaugeTimer >= 1.0f)
             {
-                hit.transform.GetComponent<Button>().onClick.Invoke();
+                if((PlayerPrefs.GetInt("SceneNum") == 0) || (PlayerPrefs.GetInt("SceneNum") == 1))
+                {
+                    hit.transform.GetComponent<Button>().onClick.Invoke();
+                }
                 GaugeTimer = 0.0f;
+            }
+
+            else if (PlayerPrefs.GetInt("SceneNum") == 2)
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    transform.Translate(mainCam.transform.TransformDirection(Vector3.forward) * 7.0f * Time.deltaTime); //메인카메라가 바라보는 방향으로 플레이어 이동
+
+                    positionY = this.transform.position.y;
+
+                    if (positionY != 4.5f) //플레이어의 y위치를 고정
+                        positionY = 4.5f;
+                    this.transform.position = new Vector3(transform.position.x, positionY, transform.position.z);
+                }
             }
         }
         else
