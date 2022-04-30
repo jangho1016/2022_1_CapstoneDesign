8d9b013a08d2b5ae2d684567fd9391ead5df3580
jangho1016 <dhwkdgh12@gmail.com>
2022-04-30 21:34:51 +0900

    commit

---
+++ CapstoneDesign/Assets/Scripts/Animation/Default.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Default : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("rand_num", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
