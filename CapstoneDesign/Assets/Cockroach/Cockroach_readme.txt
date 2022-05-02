46bcf0547f30919932bd0d24679e3493e1577f3f
jangho1016 <dhwkdgh12@gmail.com>
2022-05-02 14:40:40 +0900

    commit

---
+++ CapstoneDesign/Assets/Cockroach/Cockroach_readme.txt
@@ -0,0 +1,28 @@
+Cockroach 3D model
+Root motion.
+
+8 Animations
+2 Textures (2048x2048)
+1 Prefabs
+1 Animator controller
+1 Script
+
+Low poly model
+1994 quad polygons on this mesh
+1996 vertices on this mesh
+
+Textures
+cockroach_D.png
+cockroach_N.png
+
+Animations List
+0-24 cockroach_walk
+27-51 cockroach_run
+54-150 cockroach_idle
+153-189 cockroach_turn_right
+192-228 cockroach_turn_left
+231-254 cockroach_death
+257-280 cockroach_attack
+283-295 cockroach_hit
+
+enjoy:)
---
+++ CapstoneDesign/Assets/Cockroach/Scripts/Cockroach.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cockroach : MonoBehaviour {
+    Animator cockroach;
+    public IEnumerator coroutine;
+
+	// Use this for initialization
+	void Start () {
+        cockroach = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            cockroach.SetBool("idle", true);
+            cockroach.SetBool("walk", false);
+        }
+        if (Input.GetKey("down"))
+        {
+            cockroach.SetBool("walk", true);
+            cockroach.SetBool("idle", false);
+            cockroach.SetBool("run", false);
+        }
+        if (Input.GetKey("up"))
+        {
+            cockroach.SetBool("run", true);
+            cockroach.SetBool("walk", false);
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            cockroach.SetBool("hit", true);
+            cockroach.SetBool("walk", false);
+            cockroach.SetBool("run", false);
+            cockroach.SetBool("idle", false);
+            StartCoroutine("idle2");
+            idle2();
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            cockroach.SetBool("attack", true);
+            cockroach.SetBool("idle", false);
+            cockroach.SetBool("walk", false);
+            cockroach.SetBool("run", false);
+            StartCoroutine("walk2");
+            walk2();
+        }
+        if (Input.GetKey(KeyCode.Alpha4))
+        {
+            cockroach.SetBool("death", true);
+            cockroach.SetBool("walk", false);
+            cockroach.SetBool("run", false);
+        }
+        if (Input.GetKey("right"))
+        {
+            cockroach.SetBool("turn_right", true);
+            cockroach.SetBool("walk", false);
+            cockroach.SetBool("run", false);
+            StartCoroutine("walk");
+            walk();
+        }
+        if (Input.GetKey("left"))
+        {
+            cockroach.SetBool("turn_left", true);
+            cockroach.SetBool("walk", false);
+            cockroach.SetBool("run", false);
+            StartCoroutine("walk");
+            walk();
+        }
+    }
+    IEnumerator walk()
+    {
+        yield return new WaitForSeconds(1.0f);
+        cockroach.SetBool("walk", true);
+        cockroach.SetBool("turn_right", false);
+        cockroach.SetBool("turn_left", false);
+        cockroach.SetBool("attack", false);
+    }
+
+    IEnumerator idle()
+    {
+        yield return new WaitForSeconds(2.0f);
+        cockroach.SetBool("idle", true);
+        cockroach.SetBool("walk", false);
+        cockroach.SetBool("hit", false);
+        cockroach.SetBool("hit", false);
+    }
+
+    IEnumerator idle2()
+    {
+        yield return new WaitForSeconds(0.2f);
+        cockroach.SetBool("idle", true);
+        cockroach.SetBool("walk", false);
+        cockroach.SetBool("hit", false);
+        cockroach.SetBool("hit", false);
+    }
+
+    IEnumerator walk2()
+    {
+        yield return new WaitForSeconds(0.5f);
+        cockroach.SetBool("walk", true);
+        cockroach.SetBool("turn_right", false);
+        cockroach.SetBool("turn_left", false);
+        cockroach.SetBool("attack", false);
+    }
+}
