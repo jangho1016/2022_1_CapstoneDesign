1b6776111bc3be3097f5f2e27ef0b69eec59b14c
hgs9590 <55690198+hgs9590@users.noreply.github.com>
2022-05-04 21:04:42 +0900

    commit

--- CapstoneDesign/Assets/QuizBTCtrl.cs
+++ CapstoneDesign/Assets/QuizBTCtrl.cs
@@ -343,7 +343,7 @@ public class QuizBTCtrl : MonoBehaviour
 
         select.text = "";
 
-        answer.DOText("정답입니다! 1문제 남았습니다\n\n\n\n\n\n", 2.0f);
+        answer.DOText("정답입니다! 1문제 남았습니다\n\n\n\n", 2.0f);
         yield return new WaitForSeconds(3.0f);
         answer.text = "";
         yield return new WaitForSeconds(2.0f);
